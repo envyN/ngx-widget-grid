287bd3e51601c02c343cde68e8f8ee7a4ae00e10
Naveen Adarsh Petla <npetla@vmware.com>
2017-07-30 22:26:51 +0530

    Simple demo commit push to gh-pages

--- styles.bundle.js
+++ styles.bundle.js
@@ -429,7 +429,7 @@ exports = module.exports = __webpack_require__(27)();
 
 
 // module
-exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\n", ""]);
+exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\nhtml, body {\n  height: 100%;\n}\n", ""]);
 
 // exports
 
