5d24708e4ca04a7716c457a67c14bbc176c912f2
Naveen Adarsh Petla <nvnpetla@amazon.com>
2022-09-17 12:09:16 +0530

    Upgrade to Angular 11

--- src/app/components/fixed-dimension-example/fixed-dimension-example.component.spec.ts
+++ src/app/components/fixed-dimension-example/fixed-dimension-example.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FixedDimensionExampleComponent } from './fixed-dimension-example.component';
 
@@ -6,7 +6,7 @@ describe('BasicExampleComponent', () => {
   let component: FixedDimensionExampleComponent;
   let fixture: ComponentFixture<FixedDimensionExampleComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [FixedDimensionExampleComponent ]
     })
